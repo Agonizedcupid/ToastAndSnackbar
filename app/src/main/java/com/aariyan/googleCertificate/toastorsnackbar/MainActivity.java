92722e1c47d32b6e8df90592ad2acb26a414e0f0
agoni <agonizedcupid@gmail.com>
2019-04-14 15:15:07 +0600

    Completed Custom Toast with Background and a simple Toast

--- app/src/main/java/com/aariyan/googleCertificate/toastorsnackbar/MainActivity.java
+++ app/src/main/java/com/aariyan/googleCertificate/toastorsnackbar/MainActivity.java
@@ -2,6 +2,11 @@ package com.aariyan.googleCertificate.toastorsnackbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Simple Toast :
+        //Toast.makeText(this, "Simple Toast", Toast.LENGTH_SHORT).show();
+
+        //Custom Toast
+        showCustomToast();
+    }
+
+    private void showCustomToast() {
+
+        //Instantiate Toast:
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.BOTTOM,0,0);
+
+        //Instantiate Custom created Layout for the Custom Toast :
+        LayoutInflater inflater = getLayoutInflater();
+        View customCreatedToastView = inflater.inflate(R.layout.custom_toast_layout,(ViewGroup)findViewById(R.id.rootLinearLayout));
+        toast.setView(customCreatedToastView);
+        toast.show();
+
     }
 }
