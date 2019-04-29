13b3d99fde643189e094c9708e431d8ab66501a9
agoni <agonizedcupid@gmail.com>
2019-04-29 16:42:10 +0600

    Snackbar is done.

--- app/src/main/java/com/aariyan/googleCertificate/toastorsnackbar/MainActivity.java
+++ app/src/main/java/com/aariyan/googleCertificate/toastorsnackbar/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
                         showCustomToast();
                     }
                 }).show();
+
         //Custom Toast
         //showCustomToast();
     }
